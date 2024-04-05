569db74909804ce6016d6fbc30d8a2586c223772
SantinoMin <santinomin0381@gmail.com>
2024-04-05 19:26:16 +0900

    Initial commit

---
+++ src/main/java/warehouseLocation/demo/WarehouseLocationApplication.java
@@ -0,0 +1,13 @@
+package warehouseLocation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/warehouseLocation/demo/WarehouseLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package warehouseLocation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
