6c98103b58c657d941daca7a66b04d063909bb3f
SantinoMin <santinomin0381@gmail.com>
2024-04-17 16:34:29 +0900

    fix: Swagger openAPI 피드백 후(by martin), 세부 정보 수정 완료.

--- src/main/java/warehouseLocation/domain/service/ProductService.java
+++ src/main/java/warehouseLocation/domain/service/ProductService.java
@@ -18,7 +18,9 @@ public class ProductService {
 
   public static ProductResDto.ProductInfo search(String productName) {
 
-    //현재 등록되어 있는 상품들을
+    //사용자가 입력한 값(=query parameters에 입력된 값)을 repository에서 찾기
+    //만약 상품명과 일치하는 상품이 있다면, 상품들 전체 보여주기
+    //근데, 검색 시, 2글자만 일치하는 경우? 이런 부분도 설정 해줘야 되는듯?
     return null;
   }
 
