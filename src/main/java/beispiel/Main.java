df0350419bd2a1f50f14f7134e2a398948af0036
SantinoMin <santinomin0381@gmail.com>
2024-05-17 13:02:26 +0900

    fix: API 수정 중..

--- src/main/java/beispiel/Main.java
+++ src/main/java/beispiel/Main.java
@@ -1,30 +1,30 @@
-//package beispiel;
-//
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-///**
-// * 디버깅 예시
-// */
+package beispiel;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 디버깅 예시
+ */
 //@SpringBootApplication
-//public class Main {
-//
-//  public static void main(String[] args) {
-//    String name = "santino";
+public class Main {
+
+  public static void main(String[] args) {
+    String name = "santino";
 //    String res = hello(name);
-//
-//    System.out.println("name =" + name);
-//
+
+    System.out.println("name =" + name);
+
 //    System.out.println("res =" + res);
-//
-//  }
-//
-//  static String hello(String name){
-////    return "hello  "+ name;
-//
-//    String mix = "hello  "+ name;
-//    System.out.println("mix = " + mix);
-//    return mix;
-//
-//  }
-//}
-//
+
+  }
+
+  static String hello(String name){
+//    return "hello  "+ name;
+
+    String mix = "hello  "+ name;
+    System.out.println("mix = " + mix);
+    return mix;
+
+  }
+}
+
--- src/main/java/warehouseLocation/domain/dto/ProductReqDto.java
+++ src/main/java/warehouseLocation/domain/dto/ProductReqDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
